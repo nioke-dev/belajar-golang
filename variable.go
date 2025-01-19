036e3c51d067f235ed6c9f4fa38237d53346b1ad
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2025-01-19 23:37:14 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string
+
+	//var name = "Muhammad Nurul Mustofa"
+	name := "Muhammad Nurul Mustofa"
+	fmt.Println(name)
+
+	name = "Nurul Mustofa"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Muhammad"
+		middleName = "Nurul"
+		lastName   = "Mustofa"
+	)
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
