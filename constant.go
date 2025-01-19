6b599e70bcadf2eee7a8f0363048d7281fe12b1a
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2025-01-19 23:42:12 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//const firstName = "Nurul"
+	//const lastName = "Mustofa"
+
+	//error
+	//firstName = "jokowi"
+	//lastName = "dodo"
+
+	const (
+		firstName = "Nurul"
+		lastName  = "Mustofa"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
