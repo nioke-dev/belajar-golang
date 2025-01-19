6a08af994228826a33622fc6e44126eb05537db7
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2025-01-19 23:26:27 +0700

    string

---
+++ string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nurul")
+	fmt.Println("Nurul Mustofa")
+	fmt.Println("Muhammad Nurul Mustofa")
+
+	//	len untuk menghitung jumlah karakter di string
+	// "string"[number] Mengambil karakter pada posisi yang ditentukan
+	fmt.Println(len("Nurul"))
+	fmt.Println("Nurul Mustofa"[0])
+	fmt.Println("Muhammad Nurul Mustofa"[1])
+}
