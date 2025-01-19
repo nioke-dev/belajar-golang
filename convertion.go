045eba4257944ed7697807bcacef7d97aefef8b2
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2025-01-19 23:49:41 +0700

    convertion data

---
+++ convertion.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var nilai32 int32 = 32768
+	var nilai64 int64 = int64(nilai32)
+	var nilai16 int16 = int16(nilai32)
+
+	println(nilai32)
+	println(nilai64)
+	println(nilai16)
+
+	var name = "Nurul Mustofa"
+	var e = name[0]
+	var estring = string(e)
+
+	println(name)
+	println(e)
+	println(estring)
+}
